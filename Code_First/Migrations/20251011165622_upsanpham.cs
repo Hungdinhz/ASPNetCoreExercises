71b356120d19ecf7a6f83c5fa96a2ca6b872fdd9
Hungdinhz <phunghung12112005@gmail.com>
2025-10-12 00:02:18 +0700

    update interface

---
+++ Code_First/Migrations/20251011165622_upsanpham.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Code_First.Migrations
+{
+    /// <inheritdoc />
+    public partial class upsanpham : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "MaLoai",
+                table: "SanPhams",
+                type: "varchar(10)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<byte>(
+                name: "TrangThai",
+                table: "SanPhams",
+                type: "tinyint",
+                nullable: false,
+                defaultValue: (byte)0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "MaLoai",
+                table: "SanPhams");
+
+            migrationBuilder.DropColumn(
+                name: "TrangThai",
+                table: "SanPhams");
+        }
+    }
+}
--- Code_First/Models/SanPham.cs
+++ Code_First/Models/SanPham.cs
@@ -18,7 +18,14 @@ namespace Code_First.Models
         public decimal DonGia { get; set; }
         public string HinhAnh { get; set; } = string.Empty;
         public int SoLuong { get; set; }
-        public LoaiSanPham LoaiSanPham { get; set; }
-        public ICollection<CTHoaDon> CTHoaDons { get; set; }
+
+        [Column(TypeName = "varchar(10)")]
+        public string MaLoai { get; set; } = string.Empty;
+
+        [Column(TypeName = "tinyint")]
+        public int TrangThai { get; set; }
+
+        public LoaiSanPham LoaiSanPham { get; set; } = new LoaiSanPham();
+        public ICollection<CTHoaDon> CTHoaDons { get; set; } = new List<CTHoaDon>();
     }
 }
