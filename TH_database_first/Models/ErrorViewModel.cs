b6720029a0a64085b1ad78a9c8ae0493be2e51c6
Hungdinhz <phunghung12112005@gmail.com>
2025-10-14 23:04:09 +0700

    delete bai cu thay bang bai databasefirst moi

--- TH_database_first/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace TH_database_first.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
